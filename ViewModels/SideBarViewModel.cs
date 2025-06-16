cc376f668f4bf7103c5ab8e349fef0fc51146489
Chris Jacklin <chris@kalopsic-visions.uk>
2025-06-16 02:43:04 +0100

    wip - sidebar styling and basic button selection code

--- ViewModels/SideBarViewModel.cs
+++ ViewModels/SideBarViewModel.cs
@@ -1,6 +1,77 @@
-﻿namespace CrossMediaPlayer.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CrossMediaPlayer.ViewModels;
 
 public partial class SideBarViewModel : ViewModelBase
 {
+    public SideBarViewModel()
+    {
+        _mediaLibraryButtonSelected = true;
+    }
     
+    [ObservableProperty]
+    private bool _mediaLibraryButtonSelected;
+    
+    [ObservableProperty]
+    private bool _artistsButtonSelected;
+    
+    [ObservableProperty]
+    private bool _albumsButtonSelected;
+    
+    [ObservableProperty]
+    private bool _playlistsButtonSelected;
+    
+    [ObservableProperty]
+    private bool _optionsButtonSelected;
+
+
+    [RelayCommand]
+    public void MediaLibraryButtonClick()
+    {
+        ResetButtonsSelected();
+
+        MediaLibraryButtonSelected = true;
+    }
+    
+    [RelayCommand]
+    public void ArtistsButtonClick()
+    {
+        ResetButtonsSelected();
+        
+        ArtistsButtonSelected = true;
+    }
+    
+    [RelayCommand]
+    public void AlbumsButtonClick()
+    {
+        ResetButtonsSelected();
+        
+        AlbumsButtonSelected = true;
+    }
+    
+    [RelayCommand]
+    public void PlaylistsButtonClick()
+    {
+        ResetButtonsSelected();
+        
+        PlaylistsButtonSelected = true;
+    }
+    
+    [RelayCommand]
+    public void OptionsButtonClick()
+    {
+        ResetButtonsSelected();
+        
+        OptionsButtonSelected = true;
+    }
+
+    private void ResetButtonsSelected()
+    {
+        MediaLibraryButtonSelected = false;
+        ArtistsButtonSelected = false;
+        AlbumsButtonSelected = false;
+        PlaylistsButtonSelected = false;
+        OptionsButtonSelected = false;
+    }
 }
