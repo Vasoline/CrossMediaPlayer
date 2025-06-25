a84d2e0fd40080aed2fdfa1e303e214ef43e109b
Chris Jacklin <chris@kalopsic-visions.uk>
2025-06-25 02:11:24 +0100

    wip - just started

--- ViewModels/Pages/MediaFoldersPageViewModel.cs
+++ ViewModels/Pages/MediaFoldersPageViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace CrossMediaPlayer.ViewModels.Pages;
+﻿using CrossMediaPlayer.Services.Translation;
+
+namespace CrossMediaPlayer.ViewModels.Pages;
 
 public partial class MediaFoldersPageViewModel : ViewModelBase
 {
-    
+    public ITranslationService TranslationService { get; }
+    public MediaFoldersPageViewModel(ITranslationService translationService)
+    {
+        TranslationService = translationService;
+    }
 }
